10066d83e3b8e9ecbd7847dcaccda8a124e6ed6a
huangwf <huangwf@akulaku.com>
2018-08-15 17:17:33 +0800

    归还连接

--- src/main/java/org/meizhuo/rpc/client/ConnectFactory.java
+++ src/main/java/org/meizhuo/rpc/client/ConnectFactory.java
@@ -10,12 +10,15 @@ import io.netty.handler.codec.string.StringDecoder;
 import org.apache.commons.pool2.BasePooledObjectFactory;
 import org.apache.commons.pool2.PooledObject;
 import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class ConnectFactory extends BasePooledObjectFactory<Channel> {
 
     private String ip;
     private Integer port;
+//    private Logger logger=LoggerFactory.getLogger(ConnectFactory.class);
 
     public ConnectFactory(String ip, Integer port) {
         this.ip = ip;
@@ -41,6 +44,22 @@ public class ConnectFactory extends BasePooledObjectFactory<Channel> {
                     }
                 });
         ChannelFuture f=b.connect(ip,port).sync();
+        //            f.addListener(new ChannelFutureListener() {
+//                @Override
+//                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    /*
+                     * sync内部会在NIO线程wait阻塞 导致这个接口回调时阻塞和唤醒可能都在同一线程造成死锁
+                     * 不论是用户直接关闭或者eventLoop的轮询状态关闭，都会在eventLoop的线程内完成notify动作，所以不要在IO线程内调用future对象的sync或者await方法
+                     * 应用程序代码都是编写的channelHandler，而channelHandler是在eventLoop的线程内执行的，所以是不能在channelHandler中调用sync或者await方法的
+                     * channel.closeFuture()不做任何操作，只是简单的返回channel对象中的closeFuture对象，对于每个Channel对象，都会有唯一的一个CloseFuture，用来表示关闭的Future，
+                     所有执行channel.closeFuture().sync()就是执行的CloseFuture的sync方法，从上面的解释可以知道，这步是会将当前线程阻塞在CloseFuture上
+                     */
+//                    f.channel().closeFuture().sync();//应用程序会一直等待，直到channel关闭 这句会造成死锁异常BlockingOperationException
+//                }
+//            });
+
+        System.out.println("pool create channel "+ip+":"+port);
+//        logger.info("pool create channel "+ip+":"+port);
         return f.channel();
     }
 
--- src/main/java/org/meizhuo/rpc/client/RPCRequestNet.java
+++ src/main/java/org/meizhuo/rpc/client/RPCRequestNet.java
@@ -38,6 +38,8 @@ public class RPCRequestNet {
     public Map<String,Deferred> promiseMap=new ConcurrentHashMap<String,Deferred>();
     //每个IP对应一个锁 防止重复连接一个IP多次
     public Map<String,Lock> connectlock=new ConcurrentHashMap<String,Lock>();
+    //每个ip对应一个连接池
+    public Map<String,ConnectionPool> connectionPoolMap=new ConcurrentHashMap<String,ConnectionPool>();
     //服务名称 映射 服务信息类
     public Map<String,ServiceInfo> serviceNameInfoMap=new ConcurrentHashMap<>();
     //IP地址 映射 对应的NIO Channel及其引用次数
@@ -52,63 +54,16 @@ public class RPCRequestNet {
         loadBalance=RPC.getClientConfig().getLoadBalance();
     }
 
-    //负载均衡获取对应IP 端口后发起连接
-    private Channel connect(String ip){
-        Channel channel=IPChannelMap.get(ip).getChannel();
-        if (channel==null){
-            //尚未有channel则新连接 每个IP加单独的锁 防止重复连接
-            Lock lock=new ReentrantLock();
-            connectlock.putIfAbsent(ip,lock);
-            connectlock.get(ip).lock();
-            if (IPChannelMap.get(ip).getChannel()==null){
-                String[] IPArr=ip.split(":");
-                String host=IPArr[0];
-                Integer port=Integer.valueOf(IPArr[1]);
-                //netty线程组
-                EventLoopGroup group=new NioEventLoopGroup();
-                //启动辅助类 用于配置各种参数
-                Bootstrap b=new Bootstrap();
-                b.group(group)
-                        .channel(NioSocketChannel.class)
-                        .option(ChannelOption.TCP_NODELAY,true)
-                        .handler(new ChannelInitializer<SocketChannel>() {
-                            @Override
-                            protected void initChannel(SocketChannel socketChannel) throws Exception {
-                                socketChannel.pipeline().addLast(new LineBasedFrameDecoder(2048));//以换行符分包 防止粘包半包 2048为最大长度 到达最大长度没出现换行符则抛出异常
-                                socketChannel.pipeline().addLast(new StringDecoder());//将接收到的对象转为字符串
-                                //添加相应回调处理和编解码器
-                                socketChannel.pipeline().addLast(new RPCRequestHandler());
-                            }
-                        });
-                try {
-                    //同步等待连接成功
-                    ChannelFuture f=b.connect(host,port).sync();
-                    channel=f.channel();
-                    //将连接成功的通道放入Map
-                    IPChannelMap.get(ip).setChannel(channel);
-                    IPChannelMap.get(ip).setGroup(group);
-//            f.addListener(new ChannelFutureListener() {
-//                @Override
-//                public void operationComplete(ChannelFuture channelFuture) throws Exception {
-                    /*
-                     * sync内部会在NIO线程wait阻塞 导致这个接口回调时阻塞和唤醒可能都在同一线程造成死锁
-                     * 不论是用户直接关闭或者eventLoop的轮询状态关闭，都会在eventLoop的线程内完成notify动作，所以不要在IO线程内调用future对象的sync或者await方法
-                     * 应用程序代码都是编写的channelHandler，而channelHandler是在eventLoop的线程内执行的，所以是不能在channelHandler中调用sync或者await方法的
-                     * channel.closeFuture()不做任何操作，只是简单的返回channel对象中的closeFuture对象，对于每个Channel对象，都会有唯一的一个CloseFuture，用来表示关闭的Future，
-                     所有执行channel.closeFuture().sync()就是执行的CloseFuture的sync方法，从上面的解释可以知道，这步是会将当前线程阻塞在CloseFuture上
-                     */
-//                    f.channel().closeFuture().sync();//应用程序会一直等待，直到channel关闭 这句会造成死锁异常BlockingOperationException
-//                }
-//            });
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }else {
-                channel=IPChannelMap.get(ip).getChannel();
-            }
-            connectlock.get(ip).unlock();
+    //负载均衡获取对应IP 从连接池中获取连接channel
+    private Channel connect(String ip) throws Exception {
+        String[] IPArr=ip.split(":");
+        String host=IPArr[0];
+        Integer port=Integer.valueOf(IPArr[1]);
+        if (connectionPoolMap.get(ip)==null){
+            ConnectionPool connectionPool = new ConnectionPool(host, port);
+            connectionPoolMap.putIfAbsent(ip, connectionPool);
         }
-        return channel;
+        return connectionPoolMap.get(ip).getChannel();
     }
 
     //单例模式 避免重复连接 构造方法中进行连接操作
@@ -127,9 +82,9 @@ public class RPCRequestNet {
     public void send(RPCRequest request) throws ProvidersNoFoundException {
         String serviceName=request.getClassName();
         String ip=loadBalance.chooseIP(serviceName);
-        Channel channel=connect(ip);
 //        System.out.println("Send RPC Thread:"+Thread.currentThread().getName());
         try {
+            Channel channel=connect(ip);
             //编解码对象为json 发送请求
             String requestJson= null;
             try {
@@ -139,6 +94,7 @@ public class RPCRequestNet {
             }
             ByteBuf requestBuf= Unpooled.copiedBuffer(requestJson.getBytes());
             channel.writeAndFlush(requestBuf);
+            connectionPoolMap.get(ip).releaseChannel(channel);
 //            System.out.println("调用"+request.getRequestID()+"已发送");
             //挂起等待实现端处理完毕返回
             synchronized (request) {
@@ -149,20 +105,25 @@ public class RPCRequestNet {
 //            System.out.println("调用"+request.getRequestID()+"接收完毕");
         } catch (InterruptedException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public void asyncSend(RPCRequest request) throws ProvidersNoFoundException {
         String serviceName=request.getClassName();
         String ip=loadBalance.chooseIP(serviceName);
-        Channel channel=connect(ip);
-        String requestJson= null;
         try {
+            Channel channel=connect(ip);
+            String requestJson= null;
             requestJson = RPC.requestEncode(request);
+            ByteBuf requestBuf= Unpooled.copiedBuffer(requestJson.getBytes());
+            channel.writeAndFlush(requestBuf);
+            connectionPoolMap.get(ip).releaseChannel(channel);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        ByteBuf requestBuf= Unpooled.copiedBuffer(requestJson.getBytes());
-        channel.writeAndFlush(requestBuf);
     }
 }
